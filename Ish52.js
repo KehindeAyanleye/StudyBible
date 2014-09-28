c3a6b32ee105906b8a408f3e302421966adb19e7
Kehinde Ayanleye <Kehindeayanleye@gmail.com>
2014-09-28 05:11:55 +0100

    Create Ish52.js

---
+++ Ish52.js
@@ -0,0 +1,5 @@
+For thus saith the Lord, Ye have sold yourselves for nought; and ye shall be redeemed without money.
+
+How beautiful upon the mountains are the feet of him that bringeth good tidings, that publisheth peace; that bringeth good tidings of good, that publisheth salvation; that saith unto Zion, Thy God reigneth!
+
+For ye shall not go out with haste, nor go by flight: for the Lord will go before you; and the God of Israel will be your rearward. Behold, my servant shall deal prudently, he shall be exalted and extolled, and be very high. (‭Isaiah‬ ‭52‬:‭3, 7, 12-13‬ KJV).
